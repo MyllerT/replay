628788045a0edc8c035f7c87ac319577354101d1
MyllerT <millertavares@yahoo.com.br>
2024-07-15 14:55:03 -0300

    first commit

--- src/assets/data/NewsData.js
+++ src/assets/data/NewsData.js
@@ -1,43 +1,53 @@
-import img1 from '../../assets/img/img1.webp';
+import img1 from '../../assets/img/ar.avif';
+import img2 from '../../assets/img/esp.webp';
+import img3 from '../../assets/img/fla.webp';
+import img4 from '../../assets/img/vitoria.jpg';
+import img5 from '../../assets/img/img1.webp';
 
 export const newsData = [
   {
     id: 1,
-    title: 'Lamine Yamal',
-    content: 'Lamine Yamal é um dos grandes nomes da campanha finalista da Espanha na Eurocopa 2024. Com apenas 17 anos completos hoje, o atacante do Barcelona segue quebrando recordes no futebol do Velho Continente: ele é o jogador mais jovem a atuar e marcar gols pela competição continental. Em evidência no cenário do futebol europeu, a joia chama atenção pela pouca idade.',
-    image: img1 
+    title: 'Argentina bate Colômbia ',
+    content: 'Com um gol decisivo de Lautaro Martínez na prorrogação, a Argentina triunfou sobre a Colômbia por 1 a 0 e conquistou seu segundo título consecutivo na Copa América.',
+    image: img1,
+    comment: 'Com um gol decisivo de Lautaro Martínez na prorrogação, a Argentina triunfou sobre a Colômbia por 1 a 0 e conquistou seu segundo título consecutivo na Copa América. Este triunfo marca o 16º título argentino no torneio, consolidando sua posição como a maior campeã da história. Lautaro Martínez, artilheiro do torneio com cinco gols, marcou o gol do título, redimindo-se de críticas e demonstrando a determinação da equipe em alcançar a glória.'
   },
+  
   {
     id: 2,
-
-    title: 'Notícia 1',
-    content: 'Conteúdo da notícia 1',
-    comment:'hhhgghhgjjjjjjjjjj',
-    image: img1
+    title: 'Espanha vence Inglaterra',
+    content: 'A Europa é espanhola pela quarta vez. A Espanha venceu a Inglaterra por 2 a 1, neste domingo (14), no Estádio Olímpico de Berlim, e conquistou o título da Eurocopa de 2024',
+    comment: 'Oyarzabal foi o herói do título espanhol. O atacante entrou no segundo tempo e marcou o gol decisivo nos minutos finais do duelo. Antes, Nico Williams havia colocado a Espanha na frente, e Palmer deixou tudo igual para a Inglaterra. A Espanha se tornou a maior campeã da história da Eurocopa, superando a Alemanha com quatro títulos. Esta conquista histórica aconteceu em território alemão, reafirmando a dominância da Fúria no futebol europeu.',
+    image: img2
   },
+  
   {
     id: 3,
-    title: 'Notícia 2',
-    content: 'Conteúdo da notícia 2',
-    image: img1
+    title: 'Flamengo pode comprar clube da Europa',
+    content: 'A gestão de Rodolfo Landim, que está à frente do Flamengo desde 2019, sempre teve como um dos seus objetivos, a internacionalização da marca. O clube voltou a debater esse assunto. Recentemente, o Rubro-Negro estreitou as relações com o Leixões, de Portugal. No entanto, o modelo ainda não foi definido, e não necessariamente, será de compra em um primeiro momento.',
+    image: img3,
+    comment: 'Após um estudo que mostrou a valorização dos jogadores na Europa, o Flamengo está considerando a possibilidade de entrar no mercado europeu através de parcerias, com potencial futura compra de clubes. Este movimento visa aumentar a visibilidade da marca, gerar receita em euros e proporcionar uma plataforma para desenvolver jovens talentos do clube.'
   },
+  
   {
     id: 4,
-    title: 'Notícia 3',
-    content: 'Conteúdo da notícia 3',
-    image: 'https://via.placeholder.com/140'
+    title: 'Jogadores do Vitória são agredidos',
+    content: 'Os volantes Dudu e Rodrigo Andrade, do Vitória, foram agredidos a pauladas por integrantes de torcida organizada na noite de domingo (14) em bar no bairro de Canabrava, nas proximidades do Barradão, em Salvador.',
+    image: img4,
+    comment: 'O grupo que invadiu o local também danificou o carro de Dudu, além do veículo da empresária Luciana Ribeiro, moradora do bairro. Rodrigo Andrade conseguiu se esconder dos torcedores, mas Dudu chegou a trocar socos com os agressores. As informações são do G1 e da TV Bahia.'
   },
+  
   {
     id: 4,
     title: 'Notícia 3',
     content: 'Conteúdo da notícia 3content Lamine Yamal é um dos grandes nomes da campanha finalista da Espanha na Eurocopa 2024. Com apenas 17 anos completos hoje, o atacante do Barcelona segue quebrando recordes no futebol do Velho Continente: ele é o jogador mais jovem a atuar e marcar gols pela competição continental. Em evidência no cenário do futebol europeu, a joia chama atenção pela pouca idade',
-    image: 'https://via.placeholder.com/140'
+    image: img5
   },
   {
     id: 4,
     title: 'Notícia 3',
     content: 'Conteúdo da notícia 3Lamine',
-    image: 'https://via.placeholder.com/140'
+    image: img5
   },
   
 ]
